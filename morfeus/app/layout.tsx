2a42079f0768bb29ccd058ed6e49ec75c4b720b9
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2024-08-31 23:37:03 +0300

    serxerenu

---
+++ morfeus/app/layout.tsx
@@ -0,0 +1,42 @@
+// Bismillahirahmanirahim
+
+
+
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import BasicExample from "./components/nav";
+import 'bootstrap/dist/css/bootstrap.css'
+import Footer from "./components/footer";
+
+
+
+import { Alert } from "react-bootstrap";
+import { Tegihistin } from "./components/tegihistin";
+const inter = Inter({ subsets: ["latin"] });
+
+
+export const metadata: Metadata = {
+  title: "MorFeuS",
+  description: "Fikir ve Sanat Atölyesi",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body style={{background:"orange",color:"white"}} className={inter.className}>
+        
+        <BasicExample/>
+        {children}
+    
+<Tegihistin />
+
+        <Footer/>
+        </body>
+    </html>
+  );
+}
