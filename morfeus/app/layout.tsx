31fb044ae9a9bae10d5f6c2b52ff8a630df51b3d
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2024-11-19 14:15:23 +0300

    Update layout.tsx

--- morfeus/app/layout.tsx
+++ morfeus/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body style={{background:"orange",color:"white"}} className={inter.className}>
+      <body style={{background:"green",color:"white"}} className={inter.className}>
         
         <BasicExample/>
         {children}
