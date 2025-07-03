d330cfe1b9060f143a95377fed439263917574b8
Feyzi-cmyk <feyzif655@gmail.com>
2025-07-03 09:40:44 +0300

    elhamdulillahirabbulalemin

---
+++ src/codegen/wasm/wasm_codegen.asm
@@ -0,0 +1,22 @@
+; In the name of Allah, the Most Gracious, the Most Merciful
+; wasm_codegen.asm - WebAssembly code generation logic
+
+section .text
+
+; Generate code for a function
+; wasm_codegen_func(node_ptr)
+; rdi = pointer to function node
+wasm_codegen_func:
+    ; ...generate wasm code for function...
+    ret
+
+; Generate code for a variable
+; wasm_codegen_var(node_ptr)
+; rdi = pointer to variable node
+wasm_codegen_var:
+    ; ...generate wasm code for variable...
+    ret
+
+; ...add more wasm codegen functions as needed...
+
+; All praise is due to Allah, Lord of the Worlds
---
+++ src/codegen/x86_64/x86_64_codegen.asm
@@ -0,0 +1,22 @@
+; In the name of Allah, the Most Gracious, the Most Merciful
+; x86_64_codegen.asm - x86_64 code generation logic
+
+section .text
+
+; Generate code for a function
+; x86_64_codegen_func(node_ptr)
+; rdi = pointer to function node
+x86_64_codegen_func:
+    ; ...generate x86_64 code for function...
+    ret
+
+; Generate code for a variable
+; x86_64_codegen_var(node_ptr)
+; rdi = pointer to variable node
+x86_64_codegen_var:
+    ; ...generate x86_64 code for variable...
+    ret
+
+; ...add more x86_64 codegen functions as needed...
+
+; All praise is due to Allah, Lord of the Worlds
